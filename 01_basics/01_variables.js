8d2ca87cb47abd6842f5c1c0adee991facd8a951
shivam <shivam63official@gmail.com>
2026-08-14 16:18:06 +0000

    d

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,4 @@ const accountID = 54378
 let accountEmail = "shivam63@gmail.com"
 var accountPassword = "12243"
 accountCity = "Jaipur"
+console.log([accountID,accountEmail,accountPassword,accountCity])
